85c5dbdedc9a740a462ca399a90673a9051e23e9
dourotec1 <dourotec2@proton.me>
2024-12-07 08:48:59 +0100

    Initial commit

---
+++ src/data/cryptoAssets.ts
@@ -0,0 +1,115 @@
+export const cryptoAssets = [
+  {
+    name: 'Monero',
+    symbol: 'XMR',
+    balance: '0.00',
+    address: '465dTZGGuaShWF9LQcpQNTQBbutUpQRLv4oWoEeFoUJW81Np2ropX7Bf5tutQ5t6izLTSXaLPCi3D4ae4yfboLiP3wKTAwi',
+    logo: 'https://cryptologos.cc/logos/monero-xmr-logo.png?v=035'
+  },
+  {
+    name: 'Solana',
+    symbol: 'SOL',
+    balance: '0.00',
+    address: '6TmFDmhUekUuiu5WGs86nbuAjHBNF5151iLdZXWFhTD9',
+    logo: 'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/128/black/sol.png'
+  },
+  {
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    balance: '0.00',
+    address: 'bc1qm98x7ajfqnv87m025xejwp5yx947a5g08hdtwk',
+    logo: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=035'
+  },
+  {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    balance: '0.00',
+    address: '0xec50538871198336D81b9F0d5B5fbBdB302D5510',
+    logo: 'https://cryptologos.cc/logos/ethereum-eth-logo.png?v=035'
+  },
+  {
+    name: 'Tether (ERC20)',
+    symbol: 'USDT',
+    network: 'ERC20',
+    balance: '0.00',
+    address: '0xec50538871198336D81b9F0d5B5fbBdB302D5510',
+    logo: 'https://cryptologos.cc/logos/tether-usdt-logo.png?v=035'
+  },
+  {
+    name: 'USD Coin (ERC20)',
+    symbol: 'USDC',
+    network: 'ERC20',
+    balance: '0.00',
+    address: '0xec50538871198336D81b9F0d5B5fbBdB302D5510',
+    logo: 'https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=035'
+  },
+  {
+    name: 'Dai (ERC20)',
+    symbol: 'DAI',
+    network: 'ERC20',
+    balance: '0.00',
+    address: '0xec50538871198336D81b9F0d5B5fbBdB302D5510',
+    logo: 'https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png?v=035'
+  },
+  {
+    name: 'Binance Coin',
+    symbol: 'BNB',
+    network: 'BSC',
+    balance: '0.00',
+    address: '0xec50538871198336D81b9F0d5B5fbBdB302D5510',
+    logo: 'https://cryptologos.cc/logos/bnb-bnb-logo.png?v=035'
+  },
+  {
+    name: 'Tether (BEP20)',
+    symbol: 'USDT',
+    network: 'BEP20',
+    balance: '0.00',
+    address: '0xec50538871198336D81b9F0d5B5fbBdB302D5510',
+    logo: 'https://cryptologos.cc/logos/tether-usdt-logo.png?v=035'
+  },
+  {
+    name: 'USD Coin (BEP20)',
+    symbol: 'USDC',
+    network: 'BEP20',
+    balance: '0.00',
+    address: '0xec50538871198336D81b9F0d5B5fbBdB302D5510',
+    logo: 'https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=035'
+  },
+  {
+    name: 'Dai (BEP20)',
+    symbol: 'DAI',
+    network: 'BEP20',
+    balance: '0.00',
+    address: '0xec50538871198336D81b9F0d5B5fbBdB302D5510',
+    logo: 'https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.png?v=035'
+  },
+  {
+    name: 'TRON',
+    symbol: 'TRX',
+    balance: '0.00',
+    address: 'TT62e3mz6o38tXmAo96f1S424FSUb8Szta',
+    logo: 'https://cryptologos.cc/logos/tron-trx-logo.png?v=035'
+  },
+  {
+    name: 'Tether (TRC20)',
+    symbol: 'USDT',
+    network: 'TRC20',
+    balance: '0.00',
+    address: 'TT62e3mz6o38tXmAo96f1S424FSUb8Szta',
+    logo: 'https://cryptologos.cc/logos/tether-usdt-logo.png?v=035'
+  },
+  {
+    name: 'Celo',
+    symbol: 'CELO',
+    balance: '0.00',
+    address: '0xec50538871198336D81b9F0d5B5fbBdB302D5510',
+    logo: 'https://cryptologos.cc/logos/celo-celo-logo.png?v=035'
+  },
+  {
+    name: 'Celo Dollar',
+    symbol: 'cUSD',
+    balance: '0.00',
+    address: '0xec50538871198336D81b9F0d5B5fbBdB302D5510',
+    logo: 'https://cryptologos.cc/logos/celo-celo-logo.png?v=035'
+  }
+];
